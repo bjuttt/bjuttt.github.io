addd5012efd8041ddb968b6fba0932e64433d6e3
bjuttt <44982696+bjuttt@users.noreply.github.com>
2019-03-25 22:31:46 +0100

    Project configuration

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
