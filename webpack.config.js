74a6abba555e63f3317497752aeaebf253c6beda
bjuttt <44982696+bjuttt@users.noreply.github.com>
2019-04-01 23:06:16 +0200

    homework2
    part of homework no 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,72 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const distDir = path.resolve(__dirname, 'dist');
+
+console.log(distDir)
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
-    },
-    plugins: [new HtmlWebpackPlugin()]
+  entry: ['./src/index.js', './src/styles/index.scss'],
+  output: {
+    filename: 'main.js',
+    path: distDir
+  },
+  mode: 'development',
+  devtool: 'eval',
+  devServer: {
+    contentBase: distDir
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "postcss-loader"
+        },{
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              root: path.resolve(__dirname, 'src')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
